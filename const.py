c5f69033123f8608686eb2a6fbfc90ac51421d05
Marcus Croucher <marcus.crouche@virbac.co.nz>
2019-05-16 06:11:35 +0200

    Update constants file for pyxel version.

--- const.py
+++ const.py
@@ -1,32 +1,23 @@
 """Constants for the battleship game."""
 
 from collections import namedtuple
-import colorful
 
-colorful.use_style("solarized")
 
 SIZE = 9
-LINE_LENGTH = (SIZE * 3) + 3
+BOX_SIZE = 16
 EMPTY = "-"
 SHIP = "S"
 HIT = "H"
 MISS = "M"
 SUNK = "Sunk"
 
-BOARD_COLOURS = {
-    SHIP: colorful.blue,
-    HIT: colorful.red,
-    MISS: colorful.violet,
-    EMPTY: colorful.base02,
-    "recent": colorful.yellow,
-}
-
-SEPERATOR = "  "
-BOARD_SEPERATOR = colorful.cyan("  --|--  ")
-
-PROMPT = colorful.orange(">>> ")
-
 START_SHIPS = [7, 5, 4, 4, 3, 2]
 
 Point = namedtuple("Point", "x y")
 Ship = namedtuple("Ship", "position length orientation")
+
+
+### Pyxel colours #############################################
+
+C_OCEAN_LINE
+C_OCEAN
