d0ac0de8968b840071089dc6f1127c45596a23ce
Marcus Croucher <marcus.crouche@virbac.co.nz>
2019-05-16 06:43:02 +0200

    Add scorebox.

--- main.py
+++ main.py
@@ -19,34 +19,45 @@ class App:
     def __init__(self):
         self.game_box = const.SIZE * const.BOX_SIZE
         pyxel.init(self.game_box, self.game_box + const.SCORE_HEIGHT)
-        self.x = 0
+
+        pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
 
     ### Update methods ############################################
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        pass
 
     ### Draw methods ##############################################
 
     def draw(self):
         self.draw_background()
+        self.draw_mouse()
 
     def draw_background(self):
         """Draw background and grid."""
         pyxel.cls(const.C_OCEAN)
 
+        game_box = self.game_box - 1
+
         # Draw lines
-        width = self.game_box - 1
         for i in range(const.SIZE):
             position1 = i * const.BOX_SIZE
             position2 = position1 + const.BOX_SIZE - 1
 
             col = const.C_OCEAN_LINE
-            pyxel.line(x1=position1, y1=0, x2=position1, y2=width, col=col)
-            pyxel.line(x1=position2, y1=0, x2=position2, y2=width, col=col)
-            pyxel.line(x1=0, y1=position1, x2=width, y2=position1, col=col)
-            pyxel.line(x1=0, y1=position2, x2=width, y2=position2, col=col)
+            pyxel.line(x1=position1, y1=0, x2=position1, y2=game_box, col=col)
+            pyxel.line(x1=position2, y1=0, x2=position2, y2=game_box, col=col)
+            pyxel.line(x1=0, y1=position1, x2=game_box, y2=position1, col=col)
+            pyxel.line(x1=0, y1=position2, x2=game_box, y2=position2, col=col)
+
+        # Draw scorebox
+        col = const.C_SCOREBOX
+        height = game_box + const.SCORE_HEIGHT
+        pyxel.rectb(x1=0, y1=game_box + 1, x2=game_box, y2=height, col=col)
+
+    def draw_mouse(self):
+        pass
 
 
 if __name__ == "__main__":
