5ec4bf63c2b5c9172c271f8f20ada02f8d05ba76
Marcus Croucher <marcus.crouche@virbac.co.nz>
2019-05-16 06:58:51 +0200

    Add selection.

--- main.py
+++ main.py
@@ -20,19 +20,41 @@ class App:
         self.game_box = const.SIZE * const.BOX_SIZE
         pyxel.init(self.game_box, self.game_box + const.SCORE_HEIGHT)
 
+        self.selection = None
         pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
 
     ### Update methods ############################################
 
     def update(self):
-        pass
+        if pyxel.btn(pyxel.KEY_Q):
+            pyxel.quit()
+        self.update_selection_box()
+
+    def update_selection_box(self):
+        """Update the selection box.
+
+        Only have a selection if the mouse is in the game box and if 
+        the mouse is not on a line."""
+
+        x, sub_x = divmod(pyxel.mouse_x, const.BOX_SIZE)
+        y, sub_y = divmod(pyxel.mouse_y, const.BOX_SIZE)
+
+        if sub_x in (0, 15) or sub_y in (0, 15):
+            self.selection = None
+            return
+
+        if not (0 <= x < const.SIZE and 0 <= y < const.SIZE):
+            self.selection = None
+            return
+
+        self.selection = x, y
 
     ### Draw methods ##############################################
 
     def draw(self):
         self.draw_background()
-        self.draw_mouse()
+        self.draw_selection()
 
     def draw_background(self):
         """Draw background and grid."""
@@ -56,8 +78,13 @@ class App:
         height = game_box + const.SCORE_HEIGHT
         pyxel.rectb(x1=0, y1=game_box + 1, x2=game_box, y2=height, col=col)
 
-    def draw_mouse(self):
-        pass
+    def draw_selection(self):
+        """Draw the selected position of the mouse."""
+
+        if self.selection:
+            x, y = self.selection
+            x, y = x * const.BOX_SIZE, y * const.BOX_SIZE
+            pyxel.rect(x1=x + 1, x2=x + 14, y1=y + 1, y2=y + 14, col=const.C_SELECTION)
 
 
 if __name__ == "__main__":
